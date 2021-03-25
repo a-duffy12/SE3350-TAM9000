27eaf18d22444959b6112e7ea419979ad6c0e703
Aiden <12aduffy@gmail.com>
2021-03-24 22:16:27 -0400

    [Feat] Frontend for submitting applications
    + Can submit TA applications as an Admin on the front end
    - Not built yet!!

--- ta-matcher/src/app/application/application.component.ts
+++ ta-matcher/src/app/application/application.component.ts
@@ -1,8 +1,8 @@
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { Subscription, interval} from 'rxjs';
 import { Validator } from '../validator.service';
-
+import { ShareDataService } from '../share-data.service';
+import { Subscription, interval, Observable } from 'rxjs';
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
@@ -10,62 +10,90 @@ import { Validator } from '../validator.service';
 })
 export class ApplicationComponent implements OnInit {
 
-  options = [{display: "Yes", value: true}, {display:"No", value: false}];
-  @Input() course: string | undefined;
-  @Input() courseTaken: Boolean | undefined;
-  @Input() courseLoc: string | undefined;
-  @Input() courseTAd: Boolean | undefined;
-  @Input() prevExp: string | undefined;
-
+  header = { 'Content-Type': 'application/json' };
+  options = ({ headers: this.header });
+  mdata = undefined;
 
   sub: Subscription;
-  activeUser = "";
-  isStudent: Boolean = false;
-  studentEmail: string | undefined;
-  taEmail: string | undefined;
+  activeUser = '';
+  inst = false;
+  admn = false;
 
-  constructor(private http: HttpClient, public validator: Validator) {
-    this.sub = interval(100).subscribe(() => {
-      this.activeUser = this.validator.getActiveUser();
+  subject = ''; // make upper case
+  catalog = ''; // make upper case
+  extension = ''; // make upper case
+  email = '';
+  name = '';
+  status = 0;
+  newTA = true; // used to select hours
+  hours = 0;
+  courseRank = 0;
+  courseName = '';
+
+  constructor(private http: HttpClient, private val: Validator, public data: ShareDataService)
+  {
+    // every second, update the active user variable
+    this.sub = interval(1000).subscribe(() => {
+      this.activeUser = this.val.getActiveUser();
 
       if (this.activeUser)
       {
-        // check to see if this user is a student
+        // check to see if this user is an instructor
         this.http.get(`/api/users/${this.activeUser}`).subscribe((data:any) => {
-          if (data.type === "student") // if user is a student
+
+          if (data.type == "instructor")
           {
-            this.isStudent = true;
-            this.studentEmail = data.email;
+            this.inst = true;
+            this.admn = false;
           }
-          else // if user is not a student
+          else if (data.type == "admin")
           {
-            this.isStudent = false;
+            this.admn = true;
+            this.inst = false;
           }
-        })
+        });
       }
       else
       {
-        this.isStudent = false;
+        this.inst = false;
+        this.admn = false;
       }
     });
-
   }
 
   ngOnInit(): void {
   }
 
-  submitApplication(){
-    if(confirm("Are you sure you want to submit this application?")){
-      this.http.post<any>(`/api/application/${this.studentEmail}`, {base: [this.course, this.courseTaken, this.courseLoc,this.courseTAd, this.prevExp]}).subscribe((data:any) => {
-        alert("Application submitted");
-      })
-    }
-  }
+  // function to sumbit a TA application
+  submitApplication(): void
+  {
+    if (this.activeUser && this.admn && this.subject && this.catalog && this.email && this.name && this.status && this.courseRank)
+    {
+      this.courseName = this.subject.toUpperCase() + this.catalog + this.extension.toUpperCase(); // create course name
 
-  deleteApplication(){
-    this.http.delete<any>(`/api/application/delete/${this.taEmail}`).subscribe((data:any) => {
-      //alert("Applications deleted");
-    })
+      if (this.newTA) // never TAed before
+      {
+        this.hours = 5;
+      }
+      else if (!this.newTA) // has TAed before
+      {
+        this.hours = 10;
+      }
+
+      let body = {
+        name: this.name,
+        status: this.status,
+        hours: this.hours,
+        courseRank: this.courseRank
+      }
+
+      this.http.post(`/api/application/${this.courseName}/${this.email}`, body, this.options).subscribe(() => {
+        alert(`Submitted a TA application for candidate ${this.email} to TA ${this.courseName}`);
+      }, (err => {
+        alert(err.error);
+      })
+      )
+    }
   }
 
 }
